72a5b02f64a66dd655948608b3a08b39e9736559
zhen <891079046@qq.com>
2016-05-31 16:23:54 +0800

    urlscanner

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require "vendor/autoload.php";
+
+$urls = [
+    "http://www.baidu.com",
+    "http://www.baidua.com"
+];
+
+$scanner =  new \QianXun\UrlScanner\Url\Scanner($urls);
+print_r($scanner->getInvalidUrls());
