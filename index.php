3cda751d79be00d302b6f419eb9d6c9f596db0bb
zhen <891079046@qq.com>
2016-06-06 10:29:18 +0800

    index.php merge

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 require "vendor/autoload.php";
 
 $urls = [
-    "http://www.baidu.com",
-    "http://www.baidua.com"
+"http://www.baidu.com",
+"http://www.baidua.com"
 ];
 
-$scanner =  new \QianXun\UrlScanner\Url\Scanner($urls);
-print_r($scanner->getInvalidUrls());
+$scanner =  new \QianXun\UrlScanner\Url\Scanner( $urls );
+print_r( $scanner->getInvalidUrls() );
